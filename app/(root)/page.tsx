a1daf9a09369f75b2bf335fe2599f36848645457
Danny <varmarid@gmail.com>
2025-09-24 18:42:12 +0300

    make some minor changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,8 @@
-import { auth } from "@/auth";
+
 import Hero from "@/components/Hero";
-import Image from "next/image";
-import Link from "next/link";
 
 export default async function Home() {
-  const session = await auth();
+
   return (
     <>
       {/* <div
