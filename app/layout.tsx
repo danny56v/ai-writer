d0bf9a728a888226ebd918a5e880e83a911275e6
Danny <varmarid@gmail.com>
2025-12-05 17:04:20 +0200

    fix pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,6 +86,7 @@ export default async function RootLayout({
     'https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '${FB_PIXEL_ID}');
     fbq('track', 'PageView');
+
   `}
   </Script>
 
@@ -98,7 +99,7 @@ export default async function RootLayout({
       height="1"
       width="1"
       style={{ display: "none" }}
-      src="https://www.facebook.com/tr?id=1105338574849911&ev=PageView&noscript=1"
+      src="https://www.facebook.com/tr?id=866575966334848&ev=PageView&noscript=1"
       alt=""
     />
   </noscript>
